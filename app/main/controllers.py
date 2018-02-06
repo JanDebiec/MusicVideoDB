0cdff4917696f6fa2a263654e0dc8eea82356ca7
jan@t460-U16 <jan.debiec@ue-mail.de>
2018-02-06 19:45:48 +0100

    message with abmount of shows and performers on start

--- app/main/controllers.py
+++ app/main/controllers.py
@@ -1,11 +1,17 @@
 from flask import Blueprint, render_template, flash, redirect, url_for
 
+from app.mod_db.models import Performer, Show
+
 mod_main = Blueprint('main', __name__)
 
 @mod_main.route('/', methods=['GET', 'POST'])
 @mod_main.route('/index', methods=['GET', 'POST'])
 def index():
-    messageText = ''
+    shows = Show.query.all()
+    amountShows = len(shows)
+    perfs = Performer.query.all()
+    amountPerfs = len(perfs)
+    messageText = 'MusicVideoDB with {} shows, {} performers'.format(amountShows, amountPerfs)
 
     return render_template('index.html',
                            message=messageText,
